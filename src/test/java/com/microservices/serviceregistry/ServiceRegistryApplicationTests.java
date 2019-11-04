d90fea8f42df5c9da71ef6a33c1a8618ac7a5d43
Dell <Dell@192.168.2.9>
2019-11-04 21:30:34 +0530

    Commit

---
+++ src/test/java/com/microservices/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.serviceregistry;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
